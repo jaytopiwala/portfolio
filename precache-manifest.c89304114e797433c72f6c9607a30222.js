c496c5f923b6b4b0d85033952c6e906917d80c15
jaytopiwala <65362911+jaytopiwala@users.noreply.github.com>
2020-08-19 10:52:16 -0500

    Updates

---
+++ precache-manifest.c89304114e797433c72f6c9607a30222.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6695e2933d97dd70ef2414a835b441bc",
+    "url": "/jaytopiwala-portfolio/index.html"
+  },
+  {
+    "revision": "50e816d149d21d897d72",
+    "url": "/jaytopiwala-portfolio/static/css/main.a0ebab53.chunk.css"
+  },
+  {
+    "revision": "2c3a6cebe25dc12e20e1",
+    "url": "/jaytopiwala-portfolio/static/js/2.514ba988.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jaytopiwala-portfolio/static/js/2.514ba988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e816d149d21d897d72",
+    "url": "/jaytopiwala-portfolio/static/js/main.aca2003a.chunk.js"
+  },
+  {
+    "revision": "b82d993e8663a304fede",
+    "url": "/jaytopiwala-portfolio/static/js/runtime-main.b93e8df9.js"
+  },
+  {
+    "revision": "9028731680cb3c2cdeec77c56664614c",
+    "url": "/jaytopiwala-portfolio/static/media/apollo.90287316.jpg"
+  },
+  {
+    "revision": "5590bcc8147ad959f08ee35fad45ba08",
+    "url": "/jaytopiwala-portfolio/static/media/banner.5590bcc8.jpeg"
+  },
+  {
+    "revision": "72488190485063d257a80b729a8a5448",
+    "url": "/jaytopiwala-portfolio/static/media/cops.72488190.jpeg"
+  },
+  {
+    "revision": "47ffa9845b383bc3d0bf6058c2591bc0",
+    "url": "/jaytopiwala-portfolio/static/media/creativeops.47ffa984.jpg"
+  },
+  {
+    "revision": "f17a76a085ff5013eba1df01f9879520",
+    "url": "/jaytopiwala-portfolio/static/media/designsprint1.f17a76a0.jpg"
+  },
+  {
+    "revision": "16ace1343f7337020aac713552c66bfa",
+    "url": "/jaytopiwala-portfolio/static/media/designsprint2.16ace134.jpg"
+  },
+  {
+    "revision": "f71a07e268dce32c683bf76709d5c5c4",
+    "url": "/jaytopiwala-portfolio/static/media/email.f71a07e2.png"
+  },
+  {
+    "revision": "1811dc00483081f1171acc69326f7e77",
+    "url": "/jaytopiwala-portfolio/static/media/google.1811dc00.jpeg"
+  },
+  {
+    "revision": "f09f5b70a84322c68df0b8e940965b3c",
+    "url": "/jaytopiwala-portfolio/static/media/headshot.f09f5b70.jpg"
+  },
+  {
+    "revision": "c7d0cdcae442c6d0aa23936bee12a301",
+    "url": "/jaytopiwala-portfolio/static/media/payments.c7d0cdca.jpeg"
+  },
+  {
+    "revision": "3793e5c6fd0fe3788c7b49861171f0c8",
+    "url": "/jaytopiwala-portfolio/static/media/pep.3793e5c6.png"
+  },
+  {
+    "revision": "0541280513fd2e5512349e70182b0e85",
+    "url": "/jaytopiwala-portfolio/static/media/projectmgmt.05412805.jpeg"
+  },
+  {
+    "revision": "ab7be956ba82882f74565fc8520cceaa",
+    "url": "/jaytopiwala-portfolio/static/media/psd2.ab7be956.gif"
+  },
+  {
+    "revision": "04724984610b0a4955a087eeec18a157",
+    "url": "/jaytopiwala-portfolio/static/media/psd2tool.04724984.png"
+  },
+  {
+    "revision": "3f9d9631cb3f0299ddb07a09c38d30cc",
+    "url": "/jaytopiwala-portfolio/static/media/sanmina.3f9d9631.png"
+  },
+  {
+    "revision": "ef8d6f3ebd923387928298d45f2c567f",
+    "url": "/jaytopiwala-portfolio/static/media/secureauth.ef8d6f3e.png"
+  }
+]);
